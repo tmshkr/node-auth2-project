be68e27fafab81ddf4a40712242ae0da7b729959
Tim Shaker <tim@tmshkr.com>
2020-04-21 12:53:24 -0700

    update findBy

--- api/users-model.js
+++ api/users-model.js
@@ -12,7 +12,7 @@ function find() {
 }
 
 function findBy(filter) {
-  return db("users").select("id", "username", "department").where(filter);
+  return db("users").where(filter);
 }
 
 async function add(user) {
